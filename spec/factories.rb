2f00236bf7e8f0bb7b4f9563e04c96c395caf03d
Carlos Ricardo Mora S nchez <cmora@cecropiasolutions.com>
2014-07-29 09:18:37 -0600

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Carlos Ricardo Mora Sanchez"
+    email    "cmora@cecropiasolutions.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
